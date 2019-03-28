fb9efe6472d806d6f0a630374ab29301347affc0
andrescastellanos112013 <andrescastellanos112013@gmail.com>
2019-03-27 20:18:33 -0500

    Is added the click event Body

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 document.onreadystatechange = () => {
-    var element = document.getElementById('test')
-    element.onclick = function (e) {
-        element.style.backgroundColor = "red"
-    }
-    
-    element.onkeydown = function (e) {
-        element.style.backgroundColor = "red"
-    }
+  var element = document.getElementById("test");
+  element.onclick = function(e) {
+    element.style.backgroundColor = "red";
+  };
 
-}
+  element.onkeydown = function(e) {
+    element.style.backgroundColor = "red";
+  };
+
+  var body = document.body;
+
+  element.style.position = "absolute";
+  body.onclick = e => {
+    console.log(e.clientX, e.clientY);
+    element.style.left = e.clientX + "px";
+    element.style.top = e.clientY + "px";
+  };
+};
